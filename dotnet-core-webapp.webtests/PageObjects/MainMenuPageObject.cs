dd256386ca6e6b10962369531fe55052ab167dd1
Rob Bos <raj.bos@gmail.com>
2020-11-11 19:21:01 +0100

    Adding web test project

---
+++ dotnet-core-webapp.webtests/PageObjects/MainMenuPageObject.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+
+namespace dotnetcorewebapp.webtests.PageObjects
+{
+    class MainMenuPageObject
+    {
+        private readonly IWebDriver Driver;
+
+        const string PageLinkText = "Privacy";
+        const string PageMainText = "Privacy Policy";
+
+        public MainMenuPageObject(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        public bool GoToPrivacyPage()
+        {
+            IWebElement element = Driver.FindElement(By.LinkText(PageLinkText));
+
+            element.Click();
+
+            element = Driver.FindElement(By.XPath($"//*[contains(text(), '{PageMainText}')]"));
+
+            return element != null;
+        }
+    }
+}
---
+++ dotnet-core-webapp.webtests/UnitTest1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using dotnetcorewebapp.webtests.PageObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace dotnetcorewebapp.webtests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private const string url = "http://localhost/dotnetcore-webapp/";
+        /// <summary>
+        /// Central store of the driver instance to use
+        /// </summary>
+        private static IWebDriver Driver;
+
+        /// <summary>
+        /// Initialization per class instance. We'll setup a new driver so that it can be reused between tests
+        /// </summary>
+        /// <param name="context"></param>
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            var options = new ChromeOptions();
+            if (!Debugger.IsAttached)
+            {
+                options.AddArguments("headless");
+            }
+            //Create the reference for our browser
+            Driver = new ChromeDriver(options);
+
+            //Navigate to start page
+            Driver.Navigate().GoToUrl(url);
+        }
+
+        /// <summary>
+        /// Test going to the index page
+        /// </summary>
+        [TestMethod]
+        public void GoToIndexPage()
+        {
+            foreach (var pageName in new string[] { "Home", "Privacy", "dotnetcore_webapp", "Home", "Privacy", "dotnetcore_webapp" })
+            {
+                IWebElement element = Driver.FindElement(By.LinkText(pageName));
+
+                element.Click();
+                Wait();
+
+                Assert.AreEqual(true, true);
+            }
+        }
+
+        /// <summary>
+        /// Test going to the about page
+        /// </summary>
+        [TestMethod]
+        public void GoToPrivacyPage()
+        {
+            Wait();
+            var pageName = "Privacy";
+            IWebElement element = Driver.FindElement(By.LinkText(pageName));
+
+            element.Click();
+
+            var findText = "Privacy Policy";
+
+            element = Driver.FindElement(By.XPath($"//*[contains(text(), '{findText}')]"));
+            Wait();
+
+            Assert.IsNotNull(element, $"Couldn't find description text '{findText}' on page '{pageName}'");
+        }
+
+        private static void Wait()
+        {
+            if (Debugger.IsAttached)
+            {
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+            }
+        }
+
+        /// <summary>
+        /// Test going to the about page by using a page object
+        /// </summary>
+        [TestMethod]
+        public void GoToPrivacyPagebyPageObject()
+        {
+            var mainMenuObject = new MainMenuPageObject(Driver);
+
+            var contactPage = mainMenuObject.GoToPrivacyPage();
+
+            Assert.IsTrue(contactPage, $"Navigation to contact page failed. Current url: '{Driver.Url}'");
+        }
+
+        /// <summary>
+        /// Cleanup selenium driver object to prevent any lingering chrome instances
+        /// </summary>
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            //Close the browser
+            Driver.Close();
+            // and actually close the chromedriver.exe
+            Driver.Quit();
+        }
+    }
+}
