c1acaa7fe71bb12eb54f3a222837c3c56769898b
Rob Bos <rajbos@users.noreply.github.com>
2020-11-12 10:07:22 +0100

    Adding webtests examples (#2)

---
+++ dotnet-core-webapp.webtests/Helpers/Configuration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace dotnetcorewebapp.webtests.Helpers
+{
+    public static class Configuration
+    {
+        public static IConfigurationRoot GetConfigurationRoot()
+        {
+            return new ConfigurationBuilder()
+                .AddJsonFile("AppSettings.json", optional: true)
+                .Build();
+        }
+
+        public static TestConfiguration GetApplicationConfiguration()
+        {
+            var configuration = new TestConfiguration();
+
+            var iConfig = GetConfigurationRoot();
+
+            ConfigurationBinder.Bind(iConfig, configuration);
+            
+            return configuration;
+        }
+    }
+
+    public class TestConfiguration
+    {
+        public string testUrl { get; set; }
+    }
+}
